972f2f403e7c17e3c41fe250bc2d41fd9c31f792
Ian Neal <pyrocater@gmail.com>
2015-02-11 15:51:42 -0700

    Include Zheng's little skeleton

---
+++ p3.c
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ *
+ * filename: p3.c
+ *
+ * description:
+ *
+ * group: SANIC TEEM
+ * authors: Ian Neal, Rob Kelly
+ * class: CSE 325
+ * instructor: Zheng
+ * assignment: Lab Project 3
+ * assigned: Feb 11 2015
+ * due: Feb 25 2015
+ ******************************************************************************/
+
+int main(int argc, char *argv[]){
+  /* 1. Get command line arguments */
+  /* 2. Initialize buffer entries with -1*/
+  /* 3. Create producer thread(s) */
+  /* 4. Create consumer thread(s) */
+  /* 5. Sleep 300 seconds*/
+  /* 6. Exit */
+}
