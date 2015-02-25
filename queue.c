9567e6cae998216263b01d630c12fac93bc43914
rob <rkelly@nmt.edu>
2015-02-25 03:11:35 -0700

    Implementation of push_queue

--- queue.c
+++ queue.c
@@ -27,6 +27,62 @@ queue* new_queue(bool is_FIFO, int capacity) {
   return q;
 }
 
+int push_queue(queue* q, int item) {
+  struct q_entry *new_entry = malloc(sizeof(struct q_entry));
+
+  if(new_entry == NULL) {
+    /* Error handling for that malloc */
+    perror("queue push"); // TODO
+    return -1;
+  }
+
+  new_entry->value = item;
+
+  if(sem_wait(&(q->producer))) {
+    /* Lock production semaphore */
+    /* This causes the thread to block until there's space in the queue */
+    
+    if(pthread_mutex_lock(&(q->buffer_mutex))) {
+      /* Lock buffer mutex */
+      /* This ensures that buffer modifications don't interfere */
+      
+      if(q->is_FIFO) {
+        /* Insert at head */
+        TAILQ_INSERT_HEAD(&(q->head), new_entry, entries);
+      } else {
+        /* Insert at tail */
+        TAILQ_INSERT_TAIL(&(q->head), new_entry, entries);
+      }
+      
+      q->size++;
+
+      if(!pthread_mutex_unlock(&(q->buffer_mutex))) {
+        /* Handle buffer mutex unlock failure */
+        perror("queue push"); // TODO
+        return -1;
+      }
+    } else {
+      /* Handle buffer mutex lock failure */
+      perror("queue push"); // TODO
+      return -1;
+    }
+    
+    if(sem_post(&(q->producer))
+       || sem_post(&(q->consumer))) {
+      /* Unlock production and consumption semaphores */
+      return q->size;
+    } else {
+      /* Handle semaphore post failure */
+      perror("queue push"); // TODO
+      return -1;
+    }
+  } else {
+    /* Handle semaphore wait failure */
+    perror("queue push"); // TODO
+    return -1;
+  }
+}
+
 void print_queue(queue* q) {
   struct q_entry *entry;
 
