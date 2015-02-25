0e2c2a29f3783b72402e6c4736330264110fed77
rob <rkelly@nmt.edu>
2015-02-25 01:58:38 -0700

    Added semaphore and mutex synchronization to queue interface.

--- queue.h
+++ queue.h
@@ -4,6 +4,8 @@
  */
 
 #include <sys/queue.h> // TAILQ macros
+#include <pthread.h> // pthread_mutex
+#include <semaphore.h>
 #include <stdbool.h> // bool, true, false
 
 typedef struct {
@@ -11,6 +13,8 @@ typedef struct {
   struct q_entry *head; // Pointer to head element, used internally
   int size; // kept accurate with bookkeeping
   int capacity; // max number of elements in the queue
+  pthread_mutex_t buffer_mutex; // Mutex to synchronize buffer operations
+  sem_t producer, consumer; // Producer/consumer semaphores
 } queue;
 
 struct q_entry {
@@ -32,23 +36,30 @@ queue* new_queue(bool is_FIFO, int capacity);
  * Add an element to the queue. FIFO queues add elements to the head of the
  * queue, and FILO queues add them to the tail.
  *
+ * Note! This function blocks until the buffer mutex is open AND the queue is
+ * not full
+ *
  * @param q     Initialized queue to which the new element will be added.
  * @param item  Element to add to the queue.
- * @return      Number of elements now in the queue, or -1 on failure.
- *              (e.g. full queue)
+ * @return      Number of elements now in the queue, or <0 on failure.
  */
 int push_queue(queue* q, int item);
 
 /**
  * Get the next element in the queue.
  *
+ * Note! This function blocks until the buffer mutex is open AND there are
+ * elements to pop from the queue.
+ *
  * @param q  Initialized queue from which to get an element.
- * @return   The next element in the queue, or -1 on failure (e.g. empty queue)
+ * @return   The next element in the queue, or <0 on failure.
  */
 int pop_queue(queue* q);
 
 /**
  * Free all space allocated to the queue, and the queue wrapper itself.
+ *
+ * Note! This function blocks until the buffer mutex is open.
  *
  * @param q  Queue to be freed.
  */
@@ -57,6 +68,8 @@ void free_queue(queue* q);
 /**
  * Print human-readable representation of queue to stdout. Does not print
  * a trailing newline character!
+ *
+ * Note! This function blocks until the buffer mutex is open.
  *
  * @param q  Queue to print out.
  */
