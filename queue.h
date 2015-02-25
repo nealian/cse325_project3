d2595a14c42d2fc295f585f49e6698e3446c042d
rob <rkelly@nmt.edu>
2015-02-25 01:26:29 -0700

    Documentation of queue functions. Also some revisions involving return status...

--- queue.h
+++ queue.h
@@ -10,6 +10,7 @@ typedef struct {
   bool is_FIFO; // True => FIFO queue, false => FILO.
   struct q_entry *head; // Pointer to head element, used internally
   int size; // kept accurate with bookkeeping
+  int capacity; // max number of elements in the queue
 } queue;
 
 struct q_entry {
@@ -17,8 +18,46 @@ struct q_entry {
   TAILQ_ENTRY(q_entry) entries;
 };
 
-queue* new_queue(bool is_FIFO);
-void push_queue(queue* q, int item);
+/**
+ * Create a new empty FIFO or FILO instance. Will allocate space, so the queue
+ * must be freed with free_queue when finished.
+ *
+ * @param is_FIFO   If true, the created queue is FIFO, if false then FILO
+ * @param capacity  Max number of elements in the queue.
+ * @return          Pointer to new queue.
+ */
+queue* new_queue(bool is_FIFO, int capacity);
+
+/**
+ * Add an element to the queue. FIFO queues add elements to the head of the
+ * queue, and FILO queues add them to the tail.
+ *
+ * @param q     Initialized queue to which the new element will be added.
+ * @param item  Element to add to the queue.
+ * @return      Number of elements now in the queue, or -1 on failure.
+ *              (e.g. full queue)
+ */
+int push_queue(queue* q, int item);
+
+/**
+ * Get the next element in the queue.
+ *
+ * @param q  Initialized queue from which to get an element.
+ * @return   The next element in the queue, or -1 on failure (e.g. empty queue)
+ */
 int pop_queue(queue* q);
+
+/**
+ * Free all space allocated to the queue, and the queue wrapper itself.
+ *
+ * @param q  Queue to be freed.
+ */
 void free_queue(queue* q);
+
+/**
+ * Print human-readable representation of queue to stdout. Does not print
+ * a trailing newline character!
+ *
+ * @param q  Queue to print out.
+ */
 void print_queue(queue* q);
